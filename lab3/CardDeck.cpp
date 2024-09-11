fc682903fbaf343bffbe3daa26c9dc0adc09a642
Pavel Vinduska <pvindusk@tesla2.eecs.utk.edu>
2024-09-10 21:27:28 -0400

    lab3

--- lab3/CardDeck.cpp
+++ lab3/CardDeck.cpp
@@ -1,1 +1,15 @@
 #include "CardDeck.h"
+
+// Constructor implementation 
+CardDeck::CardDeck() {
+    size = 52; 
+    for (int i = 0; i < size; i++) {
+        deck[i] = i + 1;  
+    }
+}
+
+
+void CardDeck::shuffle(){
+
+}
+
--- lab3/CardDeck.h
+++ lab3/CardDeck.h
@@ -1,6 +1,25 @@
+#ifndef CARDDECK_H 
+#define CARDDECK_H 
+
 #include <iostream>
 
 class CardDeck {
 
+private:
+    int size;
+    int deck[52];
+
+public:
+
+    void shuffle();
+    int getSize();
+
+    CardDeck();                 // default construcor
+    CardDeck(const CardDeck &); // copy constructor
+    ~CardDeck();                // destructor
+
+    const CardDeck& operator=(const CardDeck& ); // assignment operator
 
 };
+
+#endif
