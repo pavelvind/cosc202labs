25880a489da032b49a58bf1146ea1ea049ba7c18
pavelvind <pavelvinduska@icloud.com>
2024-10-17 09:50:16 -0400

    commit

--- lab7/nqueens.cpp
+++ lab7/nqueens.cpp
@@ -10,22 +10,63 @@ using namespace std;
 void nqueens(vector<int> &board, int col, int size)
 {
 
+  int row = board[col];
+  // Recursive case
   if (col < size)
   {
+    
+      board[col] = row; // Place queen
+      // Recursive call -> move in cols till i reach the boardsize
+      nqueens(board, col + 1, size);
+    
+  }
+  // Base case
+  else if (col == size)
+  {
+    // Board is valid
+    if(isValid){
+      print(board);
+    }
+    // If false -> go back one recursion and start the board with the qlueen one spot lower
+    else{
+      row++;
+      nqueens(board, col - 1, size);
+    }
   }
 }
-bool isValid(const vector<int> &board, int size)
+// Proper nqueen board without duplicates in row cols diagonals
+bool isValid(const vector<int> &board, int size, int col)
 {
-  vector<bool> duplicate;
-  duplicate.resize(size);
-
+  // Row check
+  vector<int> rowCount(size, 0);
   for (int i = 0; i < size; ++i)
   {
+    if (board[i] != -1)
+    {
+      rowCount[board[i]]++;
+    }
+    if (rowCount[board[i]] > 1)
+    {
+      return false; // Duplicate row found
+    }
   }
+
+  // Diagonal check
+  for(int i = 0; i < col; ++i)
+    {
+        if(abs(board[i] - board[col]) == abs(i - col))
+            return false; // Diagonal conflict
+    }
+    
+    return true;
 }
 
 void print(const vector<int> &board)
 {
+  for (int i = 0; i < board.size(); ++i)
+  {
+    cout << board[i] << " ";
+  }
 }
 
 int main(int argc, char *argv[])
@@ -44,6 +85,7 @@ int main(int argc, char *argv[])
 
   // Initialize board vector
   vector<int> board(size, -1);
+  nqueens(board, 0, size);
 
   return 0;
 }
